e8505dee266c130e0e9704a51f4eee61d97c1c38
Fabrizio <bigfabbro93@gmail.com>
2019-12-03 00:24:14 +0100

    A* algorithm implemented (Still needed some modifies)

--- ex03/behavior.py
+++ ex03/behavior.py
@@ -1,6 +1,7 @@
 from mars_interface import *
 import random
 import intersect
+import heapq
 import math
 import numpy as np
  
@@ -15,10 +16,10 @@ class Node:
         self.parent = parent
     
 
-    def set_parent(parent):
+    def set_parent(self, parent):
         self.parent = parent
     
-    def set_f(f):
+    def set_f(self, f):
         self.f = f
     
     def get_index(self):
@@ -33,6 +34,9 @@ class Node:
     def get_G(self):
         return self.f-self.heuristic
 
+    def get_Parent(self):
+        return self.parent
+
     def __str__(self):
         return "index: "+ self.index + " heuristic: " + self.heuristic + " F: " + self.f + " parent: " + self.parent 
 
@@ -45,81 +49,81 @@ def checkIntersect(pos, wp):
        return False
     return True
 
-def euclidean_distance(p1, p2):
-    distance = np.sqrt(np.power(p1[0]-p2[0],2)+np.power(p1[1]-p2[1],2))
-    return distance
-
-
 def a_star(start_node, goal_node, list_nodes):
-    # creating the open and closed lists
-    open_list = []
-    closed_list = []
-    # Calculating the heuristic for the starting point
-    h_start_goal = euclidean_distance(start_node, goal_node)
-    # New node is created and it is inserted in open_list
-    start = Node(len(list_nodes)-1, 0, h_start_goal)
-    open_list = np.append(open_list, start)
-    # Until the open list is not empty...
-    while open_list.size > 0:
-        # Sort the open list in descending order of f = g + h
-        open_list = sorted(open_list, key=lambda x: (x.f + x.heuristic), reverse=True)
-        # Extract the minimum and remove from the open list
-        min_node = open_list[0]
-        open_list.remove(min_node)
-        # Visiting the neighbors 
+    open_list = []  # creat the open list
+    closed_list = []  # create the closed list
+    h_start = math.floor(dist(list_nodes[start_node], list_nodes[goal_node]))  # heuristic of the starting node
+    start = Node(start_node, h_start, h_start, None)  # create the starting node object
+    heapq.heappush(open_list, (start.get_f(), start))  # push the starting node in the heap
+    while len(open_list) > 0:  # until the heap is not empty...
+        current_node = (heapq.heappop(open_list))[1]  # pop the node with the minimum f
+        if current_node.get_index() == goal_node:  # if the current node is the goal node...
+            closed_list = np.append(closed_list, current_node)  # push the current node in closed list and break
+            break
+        else:
+            closed_list = np.append(closed_list, current_node)  # push the current node in the closed list
+        # discover the neighbors of the current node
         for i in range(len(list_nodes)):
-            # If a node is directly reachable from the current node, it is a neighbor of the current node
-            if checkIntersect(list_nodes[min_node.get_index()], list_nodes[i]):
-                # If the neighbor is the goal_node add to the closed_list and end the search
-                if list_nodes[i] == goal_node:
-                    node = Node(i, 0, min_node.get_G() + euclidean_distance(list_nodes[min_node.get_index()], list_nodes[i]), min_node.get_index())
-                    closed_list = np.append(closed_list, node)
-                    break       
-                else:
-                    # Checking if the neighbor is already in the open_list
-                    find = False
-                    for j in range(len(closed_list)):
-                        # if the neighbor is already in the closed_list and the value of F is higher than the current F...
-                        current_F = min_node.get_G() + closed_list[j].get_h()
-                        if closed_list[j].get_index() == i and closed_list[j].get_f() > current_F:
-                            # Update the value of the F and the parent of the node
-                            find = True
-                            closed_list[j].set_f(current_F)
-                            closed_list[j].set_parent(min_node.get_index())
-                            break
-                    # If the neighbor is not already in the closed_list, it will be pushed inside the open_list
-                    if not find:
-                        h = euclidean_distance(list_nodes[i],goal_node)
-                        neighbor = Node(i, h, min_node.get_G() + euclidean_distance(list_nodes[min_node.get_index()], list_nodes[i]) + h, min_node.get_index())
-                        open_list = np.append(open_list, neighbor)
-            # the visited node is pushed inside the closed_list
-            closed_list = np.append(closed_list, min_node)
-    logMessage(closed_list)
-    return closed_list
- 
+            ver = next((x for x in closed_list if x.get_index() == i), None)
+            if checkIntersect(list_nodes[current_node.get_index()], list_nodes[i]) and (ver is None):
+                in_open = False
+                # calculate the f of the neighbor passing by the current node
+                h_temp = math.floor(dist(list_nodes[i], list_nodes[goal_node]))
+                f_temp = h_temp+dist(list_nodes[current_node.get_index()],list_nodes[i])+current_node.get_G()
+                for j in range(len(open_list)):
+                    if open_list[j][1].get_index() == current_node.get_index():  # if the neighbor is in the open list..
+                        in_open = True
+                        if open_list[j][0] > f_temp:  # ... and the saved f is higher than the current one
+                            np.delete(open_list,j)  # delete from the open list...
+                            updated_node = Node(i,h_temp,f_temp,current_node)
+                            # ... and push the updated one
+                            heapq.heappush(open_list,(updated_node.get_f(), updated_node))
+
+                if in_open == False:  # if the neighbor is not in the open list...
+                    new_node = Node(i,h_temp,f_temp,current_node)
+                    heapq.heappush(open_list, (f_temp, new_node))  # ... and push the node in the open list
+    logMessage("%s" %closed_list)
+    path = createPath(closed_list[len(closed_list)-1])
+    return path
+
+
+def createPath(node):
+    path = []
+    parent = node.get_Parent()
+    if parent is not None:
+        path = createPath(parent)
+    path = np.append(path, node)
+    return path
+
 def dist(p1, p2):
     return np.sum(np.square(np.subtract(p1, p2)))
-
 
 
 def doBehavior(distance, marsData, waypoints, walls, pos):
     #print "doBehavior: " + str(pos[0])
     """
-    clearLines("path")
-    configureLines("path", 5, 0.2, 0.8, 0.2)
-
     for wp in waypoints:
         if checkIntersect(pos,wp):
              #print str(pos[0]) + ":" + str(pos[1]) + " to " + str(wp[0]) + ":" + str(wp[1]) + ": no intersection"
              appendLines("path", pos[0], pos[1], 0.5)
              appendLines("path", wp[0], wp[1], 0.5)
-    
 
-            
 
 #    message = "sensor:"
     """
-    goal = [4,4,0]
-    a_star(pos, goal, waypoints)
+    goal = [4, 4]
+    start = [pos[0], pos[1]]
+    waypoints = np.append(waypoints, [start], axis=0)
+    waypoints = np.append(waypoints, [goal], axis=0)
+    lenght = len(waypoints)
+    logMessage("%s" %waypoints)
+    path = a_star(lenght-2, lenght-1, waypoints)
+    clearLines("path")
+    configureLines("path", 5, 0.2, 0.8, 0.2)
+
+    for i in range(len(path)-1):
+        appendLines("path", waypoints[path[i].get_index()][0], waypoints[path[i].get_index()][1], 0.5)
+        appendLines("path", waypoints[path[i+1].get_index()][0], waypoints[path[i+1].get_index()][1], 0.5)
+
     return
 
