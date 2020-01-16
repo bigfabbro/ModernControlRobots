8a39052adf93241c3be70128019c9f1f28eb0ade
Fabrizio <bigfabbro93@gmail.com>
2020-01-16 14:10:02 +0100

    Added some comments

--- ex05/behavior.py
+++ ex05/behavior.py
@@ -4,11 +4,10 @@ import random
 import intersect
 import math
 import numpy as np
-import time
 first_time = True
 
-
 ##########################################  CONFIGURATION PARAMETERS ##################################################
+
 """
 - map_size_x: dimension of the map on the x-axis
 - map_size_y: dimension of the map on the y-axis
@@ -17,6 +16,7 @@ first_time = True
 - wheel_distance: distance between the wheels of the robot
 - radius: wheel radius
 - laser_offset: offset between the lasers (rad/s)
+- random_percentage: number of particles (in percentage) which have to be drawn randomly at each time step
 - basePos: base positions of the lasers
 - directions: directions of the lasers
 - alphas: robot-specific error parameters, which specify the error accrued with motion (in this case randomly chosen)
@@ -28,6 +28,7 @@ update_time = 0.02
 wheel_distance = 0.485751326
 radius = 0.15
 laser_offset = 0.349
+random_percentage = 0.1
 basePos = np.array([[0.320234, 0.230233],
                     [0.320234, -0.230234],
                     [-0.306395, 0.214315],
@@ -36,7 +37,6 @@ directions = np.array([0.262, -1.309, 1.833, -2.879])
 alphas = [0.2, 0.2, 1., 1.]
 
 #######################################################################################################################
-
 particles =[]
 
 def checkIntersect(pos, wp):
@@ -59,7 +59,7 @@ def dist(p1, p2):
 def getNumberOfParticles():
     return 1000
 
-def getOdometry(joystickLeft, joystickRight):
+def getOdometryMeasurement(joystickLeft, joystickRight):
     global update_time, wheel_distance, radius
     # displacement of the left(dl)/right(dr) wheel due to the input of the left/right joystick.
     # d_l and d_r are calculated by the formula:
@@ -78,28 +78,41 @@ def getOdometry(joystickLeft, joystickRight):
     return new_odometry
 
 
-def motionModel(particle, est_pos):
+def motionModel(particle, u_t):
     global alphas
+    # u_t is transformed into a sequence of three steps:
+    # a first rotation (rot_1), a translation (trans) and a second rotation rot_2
+    delta_rot_1 = math.atan2(u_t[1], u_t[0])
+    delta_trans = math.sqrt((u_t[0]) ** 2 + (u_t[1]) ** 2)
+    delta_rot_2 = (u_t[2]) - delta_rot_1
 
-    delta_rot_1 = math.atan2(est_pos[1], est_pos[0])
-    delta_trans = math.sqrt((est_pos[0])**2 + (est_pos[1])**2)
-    delta_rot_2 =(est_pos[2]) - delta_rot_1
-
+    # Here is assumed that these three parameters are affected by independent noise.
+    # In particular the values of noises are drawn from a  zero centered normal distribution and with variance
+    # dependent by vector of robot- specific alpha parameters
     rot_1 = delta_rot_1 - np.random.normal(0, abs(alphas[0] * delta_rot_1 + alphas[1] * delta_trans))
     trans = delta_trans - np.random.normal(0, abs(alphas[2] * delta_trans + alphas[3] * (delta_rot_1 + delta_rot_2)))
     rot_2 = delta_rot_2 - np.random.normal(0, abs(alphas[0] * delta_rot_2 + alphas[1] * delta_trans))
 
     new_particle = np.zeros(3)
+    # Determining the new position and orientation of the particle
     new_particle[0] = particle[0] + trans * np.cos(particle[2]+ rot_1)
     new_particle[1] = particle[1] + trans * np.sin(particle[2]+ rot_1)
     new_particle[2] = particle[2] + rot_1 + rot_2
     return new_particle
 
 def perceptionModel(particle):
+    """
+
+    :param particle: the particle of which we want to estimate the measurements
+    :return: sse: sum of square error between the actual measurements and the estimated ones (weight of the particle)
+    """
     global basePos, directions, laser_offset, laser_max_range
     sensor_pos = []
     laser_end_pos = []
     # x_sens and y_sens are the coordinates of the starting point of the i-th group of sensors in the current particle.
+    # Note: Because of our basePos (laser base positions) are calculated from the center of the robot and our particle is
+    # positioned in center of the wheels, in order to estimate the position of the laser in the current particle we
+    # need to add a 0.255 (distance between the center of the wheels and the center of the wheelchair on the x-axis).
     for i in range(len(basePos)):
         x_sens = particle[0] + np.cos(particle[2]) * (basePos[i][0]+0.255) - np.sin(particle[2]) * basePos[i][1]
         y_sens = particle[1] + np.sin(particle[2]) * (basePos[i][0]+0.255) + np.cos(particle[2]) * basePos[i][1]
@@ -119,73 +132,80 @@ def perceptionModel(particle):
             else:
                 laser_values.append(value * laser_max_range)
     # Calculating the SSE
-    quadratic_error = np.sum(np.square(distance-laser_values))
+    sse = np.sum(np.square(distance-laser_values))
 
     """
     Used to verify that the lasers are correctly represented in each particle
     
+    clearLines("path")
+    configureLines("path", 5, 0.2, 0.8, 0.2)
     for i in range(len(sensor_pos)):
         for j in range(4):
             appendLines("path", sensor_pos[i][0], sensor_pos[i][1], 0.5)
             appendLines("path", laser_end_pos[4*i+j][0], laser_end_pos[4*i+j][1], 0.5)
     """
-    return quadratic_error
+    return sse
 
 
 def normalization(w, max):
+    """
+        :param w: vector of weights (shape: num of particles)
+        :param max: max weight
+        :return: w: normalized vector of weights. The sum of all the weights is 1 and the particle with maximum weight
+                has less probability to be drawn.
+    """
     for k in range(len(w)):
         w[k] = (max+1 - w[k])/(max+1)
     w_sum = np.sum(w)
     w = np.divide(w,w_sum)
     return  w
 
-def particle_filter(particles, u_t,pos):
-    global map_size_x, map_size_y, entropy, entropy_sum, entropy_counter
+def particle_filter(particles, u_t, pos):
+    """
+    :param particles: vector of particles (shape: 3 x num of particles)
+    :param u_t: control input (length: 3) in our case is the position estimated by odometry
+    :param pos: current position of the robot (used for the first test of motion model)
+    :return: x_t: set of particles at time t (shape: 3 x num of particles)
+    """
+    global map_size_x, map_size_y, entropy, entropy_sum, entropy_counter, random_percentage
     max_weight = 0
     weights = []
     x_cap = []
-    current_entropy = 0
-    x_t = []
+    # First phase of particle filter
     for i in range(getNumberOfParticles()):
+        # Motion model is applied to the particle to predict next state of the particle
+        # Theoretically, we are sampling from the conditional probability <x_t | u_t, x_t-1>
         x_cap.append(motionModel(particles[i], u_t))
+        # Perception model is applied to the particle to obtain its current weight
+        # In this case
         weights.append(perceptionModel(particles[i]))
-        current_entropy += weights[i]*np.log(weights[i])
-        #distance = dist(pos, particles[i])
-        #weights.append(distance)
+        """
+        used to test the correctness of the motion model with the distance from the current position of the robot
+        
+        distance = dist(pos, particles[i])
+        weights.append(distance)
+        """
         if weights[i] > max_weight:
             max_weight = weights[i]
     prob = normalization(weights, max_weight)
-    """
-    if current_entropy > entropy*1.2:
-        alp1 = 0.3
-    elif current_entropy > entropy*1.4:
-        alp1 = 0.5
-    else:
-        alp1 = 0.
-    """
-    alp1 = 0.1
-    rand_particles = int(getNumberOfParticles() * alp1)
-    indeces = np.random.choice([int(i) for i in range(0, getNumberOfParticles())],
+    rand_particles = int(getNumberOfParticles() * random_percentage)
+    indices = np.random.choice([int(i) for i in range(0, getNumberOfParticles())],
                                size=getNumberOfParticles() - rand_particles, replace=True, p=prob)
-    x_t = (np.asarray(x_cap)[indeces])
-    for i in range(int(getNumberOfParticles() * alp1)):
+    x_t = (np.asarray(x_cap)[indices])
+    for i in range(rand_particles):
         x_t = np.append(x_t, [[random.uniform(-map_size_x, map_size_x), random.uniform(-map_size_y, map_size_y),
-                               random.choice([0, -np.pi, np.pi, np.pi/2, -np.pi/2])]], axis = 0 )
-
-    entropy = current_entropy
-    #logMessage("entr: %s" %entropy)
-    #new_particles_indices = np.random.choice([int(i) for i in range(0,getNumberOfParticles())], size=getNumberOfParticles(), replace=True, p=prob)
-    #x_t = np.asarray(x_cap)[new_particles_indices]
+                               random.choice([0, -np.pi, np.pi, np.pi/2, -np.pi/2])]], axis=0)
     return x_t
 
 
 def doBehavior(distance, direction, pos, marsData, waypoints, walls, joystickLeft, joystickRight):
-    global first_time, particles, map_size_x, map_size_y, timebefore, update_time
-    #pos may only be used as a reference
-    print "Real Position: " + str(pos[0]) + ":" + str(pos[1]) + ":" + str(pos[2])
+    global first_time, particles, map_size_x, map_size_y
 
+    """
+    pos may only be used as a reference
+    print "Real Position: " + str(pos[0]) + ":" + str(pos[1]) + ":" + str(pos[2])
     print "using %d particles" % getNumberOfParticles()
-    
+    """
     if first_time:
         pData = pointCloudData["particles"]
         for i in range(getNumberOfParticles()):
@@ -195,29 +215,24 @@ def doBehavior(distance, direction, pos, marsData, waypoints, walls, joystickLef
             #logMessage("original particles %s" %particles)
 
         first_time = False
-    clearLines("path")
-    configureLines("path", 5, 0.2, 0.8, 0.2)
 
-    
-    # Laser scanner positions fl, fr, bl, br
+    #print "Joystick input: (" + str(joystickLeft) + " : " +str(joystickRight) +")" + " direction: " + str(direction)
+    odom = getOdometryMeasurement(joystickLeft, joystickRight)
+    particles = particle_filter(particles, odom, pos)
 
-
-    
-    print "Joystick input: (" + str(joystickLeft) + " : " +str(joystickRight) +")" + " direction: " + str(direction)
-    new_odom = getOdometry(joystickLeft, joystickRight)
-    particles = particle_filter(particles, new_odom, pos)
-    #logMessage(" pos%s" %pos)
-    #logMessage("particles %s" %particles)
+    """
     print "Sensors:"
-    #always counterclockwise when looking from above
+    always counterclockwise when looking from above
     print "fl (%.2f, %.2f, %.2f, %.2f)" % (distance[0], distance[1],distance[2],distance[3])
     print "fr (%.2f, %.2f, %.2f, %.2f)" % (distance[4], distance[5],distance[6],distance[7])
     print "bl (%.2f, %.2f, %.2f, %.2f)" % (distance[8], distance[9],distance[10],distance[11])
     print "br (%.2f, %.2f, %.2f, %.2f)" % (distance[12], distance[13],distance[13],distance[15])
-    #Laser center is 45 deg twisted in each case
-    #Angular offset between the laser beams: 0.349 rad
+    Laser center is 45 deg twisted in each case
+    Angular offset between the laser beams: 0.349 rad
+    """
     points = []
     for p in particles:
         points.append([p[0], p[1]])
+
     #An array of points can be returned, these are drawn in the 3D scene
     return points
